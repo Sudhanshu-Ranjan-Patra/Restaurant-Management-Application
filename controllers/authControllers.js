bd9cf4bf18aee316ff74a2e752440832dfffb88c
Sudhanshu Ranjan Patra <patrasudhanshuranjan@gmail.com>
2026-01-24 23:04:22 +0530

    updated

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -15,32 +15,31 @@ const registerController = async (req, res) => {
     }
 
     const exisiting = await userModel.findOne({ email });
-    if(exisiting){
-        return res.status(500).send({
-            success: false,
-            message: 'Email Already Registered Please Login'
-        })
+    if (exisiting) {
+      return res.status(500).send({
+        success: false,
+        message: "Email Already Registered Please Login",
+      });
     }
     //Hashing password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    //Create new user 
+    //Create new user
     const user = await userModel.create({
-        userName,
-        email,
-        password: hashedPassword,
-        phone,
-        address,
-        usertype,
+      userName,
+      email,
+      password: hashedPassword,
+      phone,
+      address,
+      usertype,
     });
 
-    
     res.status(201).send({
-        success: true,
-        message: "Successfully Registered",
-        user,
-    })
+      success: true,
+      message: "Successfully Registered",
+      user,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send({
@@ -56,7 +55,7 @@ const loginController = async (req, res) => {
   try {
     const { email, password } = req.body;
     //validation
-    if (!email || !password ) {
+    if (!email || !password) {
       return res.status(500).send({
         success: false,
         message: "Please Provide email and password.",
@@ -64,39 +63,38 @@ const loginController = async (req, res) => {
     }
 
     const user = await userModel.findOne({ email });
-    if(!user){
+    if (!user) {
       return res.status(404).send({
         success: false,
         message: "User not found!",
       });
     }
     //Compare Hashed password
-    const isMatch = await bcrypt.compare(password , user.password);
-    if(!isMatch){
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
       return res.status(404).send({
         success: false,
         message: "Invalid Credentials!",
       });
     }
     //Token
-    const token = JWT.sign({ id:user._id }, process.env.JWT_SECRET, {
-      expiresIn: "7d"
+    const token = JWT.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
     });
     //user.password = undefined; //Not show even the hashed password on login details
     res.status(200).send({
       success: true,
       message: "Login Successfully.",
       token,
-      user
-    })
+      user,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send({
       success: false,
-      message: "Error in Login API"
-    })
+      message: "Error in Login API",
+    });
   }
-}
+};
 
-
-module.exports = { registerController ,loginController };
+module.exports = { registerController, loginController };

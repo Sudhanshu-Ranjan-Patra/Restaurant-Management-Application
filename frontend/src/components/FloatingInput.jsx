02cd3791c39142bd098cd35d216992fca142ccee
Sudhanshu Ranjan Patra <patrasudhanshuranjan@gmail.com>
2026-02-15 23:55:18 +0530

    new frontend

--- frontend/src/components/FloatingInput.jsx
+++ frontend/src/components/FloatingInput.jsx
@@ -1,35 +1,5 @@
 import { useState } from "react";
 
-function FloatingInput({ label, type = "text", name }) {
-  const [focused, setFocused] = useState(false);
-  const [value, setValue] = useState("");
-
-  return (
-    <div className="relative w-screen max-w-[335px] mb-3">
-      <input
-        id={name}
-        name={name}
-        type={type}
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        onFocus={() => setFocused(true)}
-        onBlur={() => setFocused(value !== "" ? true : false)}
-        placeholder=""
-        className="w-full px-3 py-3 border border-gray-300 rounded-md bg-[#F7F8F9] focus:outline-none focus:border-purple-600"
-      />
-      <label
-        htmlFor="name"
-        className={`absolute left-3 transition-all duration-200 pointer-events-none
-        ${
-          focused
-            ? "text-xs -top-2 text-[#6C25FF] bg-[#F7F8F9] px-1"
-            : "text-gray-500 top-3"
-        }`}>
-        {" "}
-        {label}
-      </label>
-    </div>
-  );
+function FloatingInput({label,type = "text", name, value, onChange,}){
+    
 }
-
-export default FloatingInput;

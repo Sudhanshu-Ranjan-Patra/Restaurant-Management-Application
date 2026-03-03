2bedba6c5bba8d60eff3739d538251731224c903
Sudhanshu Ranjan Patra <patrasudhanshuranjan@gmail.com>
2026-03-03 23:48:11 +0530

    Update FloatingInput.jsx

--- frontend/src/components/FloatingInput.jsx
+++ frontend/src/components/FloatingInput.jsx
@@ -19,20 +19,19 @@ function FloatingInput({ label, type = "text", name, value, onChange }) {
           type === "password"
             ? "current-password"
             : name === "email"
-            ? "email"
-            : name === "username" || name === "userName"
-            ? "username"
-            : undefined
+              ? "email"
+              : name === "username" || name === "userName"
+                ? "username"
+                : undefined
         }
       />
       <label
         htmlFor={name}
         className={`absolute left-3 transition-all duration-200 pointer-events-none
-        ${
-          focused
+        ${focused
             ? "text-xs -top-2 text-[#6C25FF] bg-[#F7F8F9] px-1"
             : "text-gray-500 top-3"
-        }`}>
+          }`}>
         {label}
       </label>
     </div>
